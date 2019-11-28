292288b0723d1127225eb41863f89a3192f7470d
Guillartes, Art Lynuel M <56711239+artpaper4420@users.noreply.github.com>
2019-11-28 18:56:25 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,62 +1,9 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-int first();
-int arrsize;
-void second(int []);
-void third(int []);
-void choice(int []);
-
-
-
+#include "header.h"
+void displaymenu();
+void variable();
+void conditions();
 int main()
 {
-int age [arrsize];
-    first();
-    second(age);
-    third(age);
-    choice(age);
+displaymenu();
     return 0;
-}
-int first(){
-cout << "enter array size maximum of 4"<<endl;
-cin >> arrsize;
-return (arrsize);
-}
-void second(int age[]){
-
-for (int i=0;i<=arrsize-1;i++){
-    cout << "enter value for position"<< i+1<<":";
-    cin >> age[i];
-}
-}
-void third (int age[]){
-for (int i=0;i<=arrsize-1;i++){
-    cout << "the value are"<<i+1<<":"<<age[i]<<endl;
-
-
-}
-}
-
-void choice (int age[]){
-int choice;
-system ("CLS");
-cout << "Enter the position that you want to call"<<endl;
-cin >> choice;
-if (choice==1){
-    cout << "The value in that position is"<<age[0]<<endl;
-
-}
-else if (choice==2){
-    cout << "The value in that position is "<<age[1]<<endl;
-
-}
-else if (choice==3){
-    cout << "The value in that position is"<< age[2]<<endl;
-
-}
-else if (choice==4){
-    cout<< "The value in that position is "<<age[3]<<endl;
-
-}
 }
